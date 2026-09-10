cf5992cf1630b4f08e15755c13fcdcf9b8eab9da
aziatka2027 <aziatka2027@gmail.com>
2026-09-10 22:01:39 +0500

    Update solution_14.cpp

--- 14-longest-common-prefix/solution_14.cpp
+++ 14-longest-common-prefix/solution_14.cpp
@@ -13,13 +13,17 @@ if (strs.size() == 1){
     return strs[0];
 }
 string pref = "";
+        // Идём по символам первой строки
 for (int i = 0; i < strs[0].size(); ++i){
+    // Проверяем этот символ во всех остальных строках
     for (int j = 1; j < strs.size(); ++j){
+        // Если строка короче или символ не совпадает - выходим
         if (i >= strs[j].size() || strs[j][i] != strs[0][i]){
             return pref;
         }
         
     }
+    // Все строки прошли проверку — добавляем символ
     pref += strs[0][i];
 }
        return pref; 

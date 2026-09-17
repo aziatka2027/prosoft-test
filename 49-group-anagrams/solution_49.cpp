1f8255c394ce2eeb18c1cddc233f4cd54b3a39ff
aziatka2027 <aziatka2027@gmail.com>
2026-09-17 19:59:08 +0500

    Update solution_49.cpp

--- 49-group-anagrams/solution_49.cpp
+++ 49-group-anagrams/solution_49.cpp
@@ -7,23 +7,27 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-      // Создаем словарь: ключ — отсортированная строка,
+        // Создаем словарь: ключ — отсортированная строка,
         // значение — вектор оригинальных слов-анаграмм
-    unordered_map < string, vector<string>> groups;
-      // Проходим по всем словам
-    for (const auto& el : strs) {
-      // Создаём ключ: копируем слово и сортируем буквы
-        string key = el;
-        sort(key.begin(), key.end());
-      // Добавляем оригинальное слово в группу с этим ключом
-        groups[key].push_back(el); 
-    }
-      // Собираем все группы в итоговый вектор
-    vector<vector<string>> result;
-    for (const auto& pair : groups) {
-        result.push_back(pair.second);
-    }
-    return result;
+        unordered_map<string, vector<string>> groups;
 
-}
+        // Проходим по всем словам
+        for (const auto& el : strs) {
+            // Создаём ключ: копируем слово и сортируем буквы
+            string key = el;
+            sort(key.begin(), key.end());
+
+            // Добавляем оригинальное слово в группу с этим ключом
+            groups[key].push_back(el);
+        }
+
+        // Собираем все группы в итоговый вектор
+        vector<vector<string>> result;
+
+        for (const auto& pair : groups) {
+            result.push_back(pair.second);
+        }
+
+        return result;
+    }
 };

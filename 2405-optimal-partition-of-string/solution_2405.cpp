5f09fea0ad8c4dc5f63441498ba282a4c5156bc3
aziatka2027 <aziatka2027@gmail.com>
2026-09-17 20:02:17 +0500

    Update solution_2405.cpp

--- 2405-optimal-partition-of-string/solution_2405.cpp
+++ 2405-optimal-partition-of-string/solution_2405.cpp
@@ -5,18 +5,22 @@ using namespace std;
 class Solution {
 public:
     int partitionString(string s) {
-      // Множество для символов текущей подстроки
-    unordered_set<char> temp{};
-    int count = 1;
-    for (const auto& el : s) {
-      // Если символ уже есть в текущей подстроке — начинаем новую
-        if (temp.count(el)) {
-            ++count;
-            temp.clear();            
-        }  
-      // Добавляем символ в текущую подстроку
-        temp.insert(el);      
+        // Множество для символов текущей подстроки
+        unordered_set<char> temp{};
+
+        int count = 1;
+
+        for (const auto& el : s) {
+            // Если символ уже есть в текущей подстроке — начинаем новую
+            if (temp.count(el)) {
+                ++count;
+                temp.clear();
+            }
+
+            // Добавляем символ в текущую подстроку
+            temp.insert(el);
+        }
+
+        return count;
     }
-    return count;    
-}
 };

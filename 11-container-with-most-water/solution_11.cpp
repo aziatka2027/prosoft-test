454268dff0185e63fadc1634d6057959cbeec9d3
aziatka2027 <aziatka2027@gmail.com>
2026-09-17 19:56:26 +0500

    Update solution_11.cpp

--- 11-container-with-most-water/solution_11.cpp
+++ 11-container-with-most-water/solution_11.cpp
@@ -5,22 +5,25 @@ using namespace std;
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-    int maxarea = 0;
-    int left = 0;
-    int right = height.size() - 1;
-    while (left < right) {
-        int width = right - left; //Вычисляем ширину
-        int h = min(height[left], height[right]);// Вычисляем высоту
-        int area = width * h;// Вычисляем текущую площадь
-        maxarea = max(maxarea, area); //Выбираем максимальную площадь
-            //Сдвигаем меньший столбик
-        if (height[left] < height[right]) {
-            left++;
+        int maxarea = 0;
+        int left = 0;
+        int right = height.size() - 1;
+
+        while (left < right) {
+            int width = right - left; // Вычисляем ширину
+            int h = min(height[left], height[right]); // Вычисляем высоту
+            int area = width * h; // Вычисляем текущую площадь
+            maxarea = max(maxarea, area); // Выбираем максимальную площадь
+
+            // Сдвигаем меньший столбик
+            if (height[left] < height[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
         }
-        else {
-            right--;
-        }
+
+        return maxarea;
     }
-    return maxarea;
-}
 };
